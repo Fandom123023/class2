1442fcffea6157f7181c91cc869128900bf4435b
Tural <musaev.tural.94@mail.ru>
2024-05-08 17:33:21 +0300

    class2

--- src/Author.java
+++ src/Author.java
@@ -15,21 +15,20 @@ class Author {
         public String getLastName() {
             return lastName;
         }
+    public String toString() {
+        return "Автор: " + this.firstName + " " + this.lastName;
+    }
 
-        public String toString() {
-            return "Автор: " + this.firstName + " " + this.lastName;
-        }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) &&
+                Objects.equals(lastName, author.lastName);
+    }
 
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Author author = (Author) o;
-            return Objects.equals(firstName, author.firstName) &&
-                    Objects.equals(lastName, author.lastName);
-        }
-
-        public int hashCode() {
-            return Objects.hash(firstName, lastName);
-        }
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
     }
 
